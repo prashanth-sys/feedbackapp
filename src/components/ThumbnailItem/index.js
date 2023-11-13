a373eae0c0dfb5deb313d68646ddb3a12cc5dff4
Your Name <reddyprashanth512@gmail.com>
2023-11-13 09:43:51 +0000

    solution

---
+++ src/components/ThumbnailItem/index.js
@@ -0,0 +1,21 @@
+// Write your code here.
+// Write your code here.
+
+import './index.css'
+
+const ThumbnailItem = props => {
+  const {imageDetailsList, updateCtiveImage} = props
+  const {thumbnailUrl, id, thumbnailAltText} = imageDetailsList
+  const changeImage = () => {
+    updateCtiveImage(id)
+  }
+
+  return (
+    <li className="list-items">
+      <button className="button" type="button" onClick={changeImage}>
+        <img src={thumbnailUrl} alt={thumbnailAltText} />
+      </button>
+    </li>
+  )
+}
+export default ThumbnailItem
